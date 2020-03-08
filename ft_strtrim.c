358892e6093fd08f29c9f911d849920e4e4e6c92
Yevgeny Peliukov <yepeliuk@e1z3r10p8.42.us.org>
2020-03-08 00:22:03 -0800

    clean spaces

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,102 +6,74 @@
 /*   By: yepeliuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 21:19:35 by yepeliuk          #+#    #+#             */
-/*   Updated: 2020/02/22 22:41:42 by yepeliuk         ###   ########.fr       */
+/*   Updated: 2020/03/07 21:13:32 by yepeliuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-int		ft_isprint_mod(int c)
+static int		ft_isprint_mod(int c)
 {
 	if (c > 32 && c < 127)
 		return (1);
 	return (0);
 }
 
-int     ft_len(char *s)
-{
-  int i;
-  int  space;
-
-  i = 0;
-  space = 0;
-  while (*s != '\0')
-  {
-    if (ft_isprint_mod(*s) == 1)
-      i++;
-      s++;
-  }
-  return (i);
-}
-
-int			ft_count_words(char *s)
+static int				ft_obstr(char const *s)
 {
 	int i;
-	int j;
-	int count;
+	int index;
 
 	i = 0;
-	j = 0;
-	count = 0;
+	index = 0;
 	while (s[i] != '\0')
 	{
-		if (ft_isprint_mod(s[i]) == 0)
-        j++;
-		else if ((ft_isprint_mod(s[i]) == 1) && (j > 0))
-		{
-			count++;
-			j = 0;
-		}
+		if (ft_isprint_mod(s[i]) == 1)
+			index = i;
 		i++;
-  }
-	return (count);
+	}
+	return (index);
 }
 
-char        *ft_strtrim(char *s)
+static int				ft_size(char const *s)
 {
-    int     size;
-    int     count;
-    char    *ptr;
-    int j;
+	int i;
 
-    count = 0;
-    j = 0;
-    size = ft_len(s) + (ft_count_words(s) * 2);
-    ptr = (char *)malloc(size * sizeof(char));
-    if (ptr == NULL)
-        return (NULL);
-    while (*s != '\0')
-    {
-      if (ft_isprint_mod(*s) == 1)
-          {
-            *ptr = *s;
-            count++;
-          }
-      else if (ft_isprint_mod(*s) == 0 && (count > 0))
-        {
-          *ptr = ' ';
-          count = 0;
-        }
-        ptr++;
-        s++;
-    }
-    *ptr = '\0';
-    return (ptr);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (ft_isprint_mod(s[i]) == 1)
+			break ;
+		i++;
+	}
+	return (i);
 }
 
-int main() 
+char			*ft_strtrim(char const *s)
 {
-  char *s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
-  int ret = ft_count_words(s1);
-  char *s2 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
-  int ret1 = ft_len(s2);
-  char *a =  ft_strtrim(s1);
+	int		i;
+	int		size;
+	int		count;
+	char	*ptr;
 
-  printf("%s\n", a);
-  printf("%d\n",ret);
-  printf("%d\n",ret1);
-  return 0;
+	size = (ft_obstr(s) - ft_size(s)) + 2;
+	if (size <= 0)
+		size = 1;
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = ft_size(s);
+	count = 0;
+	if (size > 1)
+	{
+		size = (size + i) - 1;
+		while (i < size)
+		{
+			ptr[count] = s[i];
+			count++;
+			i++;
+		}
+	}
+	ptr[count] = '\0';
+	return (ptr);
 }
